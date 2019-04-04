f270d9649c5a5b544e552e96db3ff71f2d47577f
hulunbao <hulunbao@gmail.com>
2019-04-04 17:36:05 +0800

    range continued

---
+++ A_Tour_of_Go/range_continued.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow2 := make([]int,10)
+	for i := range pow2{
+		pow2[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow2{
+		fmt.Printf("%d\n",value)
+	}
+
+}
