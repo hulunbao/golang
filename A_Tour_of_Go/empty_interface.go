cb3f4062fbb4e007e7c1df9f832dfcd3eae501f1
hulunbao <hulunbao@gmail.com>
2019-07-12 17:42:42 +0800

    empty_interface

---
+++ A_Tour_of_Go/empty_interface.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{}
+	describe4(i)
+
+	i = 42
+	describe4(i)
+
+	i = "hello"
+	describe4(i)
+}
+
+func describe4(i interface{})  {
+	fmt.Printf("(%v,%T)",i,i)
+}
