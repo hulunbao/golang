32a0e5fb180cdeb29fd2d9ea405ffdd3e0bbcf60
hulunbao <hulunbao@gmail.com>
2019-07-13 11:23:24 +0800

    exercise rot reader

---
+++ A_Tour_of_Go/exercise_rot_reader.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct{
+	r io.Reader
+}
+
+func rot13(b byte) byte{
+	switch {
+	case 'A' <= b && b <= 'M':
+		b = b + 13
+	case 'M' < b && b <= 'Z':
+		b = b - 13
+	case 'a' <= b && b <= 'm':
+		b = b + 13
+	case 'm' < b && b <= 'z':
+		b = b - 13
+	}
+	return b
+}
+
+func (rr rot13Reader) Read(b []byte)(int ,error){
+	n,e := rr.r.Read(b)
+	for i := 0;i < n;i++{
+		b[i] = rot13(b[i])
+	}
+	return n,e
+}
+
+func main(){
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
+
+
