c164488743ecd5c6cf34e3d5b7629076eab611ad
hulunbao <hulunbao@gmail.com>
2019-07-22 23:54:36 +0800

    出错综合示例

--- learngo/errhanding/filelistingserver/web.go
+++ learngo/errhanding/filelistingserver/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"golang/learngo/errhanding/filelistingserver/filelisting"
 	"log"
 	"net/http"
@@ -11,9 +12,21 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request){
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func(){
+			r := recover()
+			if r != nil{
+				log.Printf("Panic: %v", r)
+				http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer,request)
 		if err != nil{
-			log.Println("Error handling request: %s",err.Error())
+			log.Printf("Error handling request: %s",err.Error())
+
+			if userErr, ok := err.(userErr); ok {
+				http.Error(writer,userErr.Message(),http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch  {
 			case os.IsNotExist(err):
@@ -28,11 +41,19 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request){
 	}
 }
 
+type userErr interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(":8888",nil)
 	if err != nil{
 		panic(err)
 	}
+	type demo string
+	d := demo("hello")
+	fmt.Println(d)
 
 }
